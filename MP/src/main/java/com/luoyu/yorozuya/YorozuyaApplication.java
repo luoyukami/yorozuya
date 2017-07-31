94ec120ae82afeb11b6156a9f5d1e920c1b1a6f0
luoyukami <luoyukami@qq.com>
2017-07-31 22:19:20 +0800

    reset MP

--- MP/src/main/java/com/luoyu/yorozuya/YorozuyaApplication.java
+++ MP/src/main/java/com/luoyu/yorozuya/YorozuyaApplication.java
@@ -1,11 +1,9 @@
-package com.example.demo;
+package com.luoyu.yorozuya;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class YorozuyaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YorozuyaApplication.class, args);
